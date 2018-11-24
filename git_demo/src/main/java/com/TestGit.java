ddfcfb11d6e808a2035e2a06a46f59c8dcc64303
lsp <lsp@136.com>
2018-11-24 21:21:22 +0800

    ddd

--- git_demo/src/main/java/com/TestGit.java
+++ git_demo/src/main/java/com/TestGit.java
@@ -6,5 +6,7 @@ public class TestGit {
         System.out.println("ssss");
         System.out.println("的点点滴滴");
         System.out.println("爱仕达多撒多撒");
+
+        System.out.println("idea");
     }
 }
